e4ed138e07f4538fc139620e6f991caa87281b37
ShariqueAzam <student1@CR33S30I.local>
2014-04-27 04:53:26 +0800

    Initial Commit

---
+++ Login Application/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Login Application
+//
+//  Created by student1 on 4/27/14.
+//  Copyright (c) 2014 student1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
